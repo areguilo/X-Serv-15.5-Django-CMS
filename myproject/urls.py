46a71da885944c33176200a8193c7b5e72c1fa93
Alejandro Reguilón Escalona <elregui.1996@gmail.com>
2018-04-28 18:48:44 +0200

    final

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
+    url(r'^$', 'cms.views.mainPage'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(.*)', 'cms.views.contentPage')
 )
