c3f07b9c7f9832383f8295b3910601472a58d2db
Alejandro Reguilón Escalona <elregui.1996@gmail.com>
2018-04-28 12:40:24 +0200

    version1

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
 # Create your views here.
+def mainPage(request):
+    list = Pages.objects.all()
+    req = '<ul>'
+    for item in list:
+        req = req + '<li><a ref=' + item.name + '</a>'
+    req = req + '</ul>'
+    return HttpResponse("<h1>Hi!, these are our contents managed:</h1>" + req)
+
+def contentPage(request, identifier):
+    print ("entra")
+    try:
+        object = Pages.objects.get(name = identifier)
+        return HttpResponse(object.page)
+    except Pages.DoesNotExist:
+        return HttpResponse("There are not pages for this object")
